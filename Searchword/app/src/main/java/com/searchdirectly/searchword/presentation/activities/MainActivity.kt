5bd7d6bb32dcd30eb57a37c0a2d2c09a7bdd1b77
barto <bartoszreterski94@gmail.com>
2023-04-05 22:26:06 +0200

    checking how works hiding icon on bottom_navigation_menu.xml

--- Searchword/app/src/main/java/com/searchdirectly/searchword/presentation/activities/MainActivity.kt
+++ Searchword/app/src/main/java/com/searchdirectly/searchword/presentation/activities/MainActivity.kt
@@ -2,19 +2,14 @@ package com.searchdirectly.searchword.presentation.activities
 
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.searchdirectly.searchword.R
 import com.searchdirectly.searchword.databinding.ActivityMainBinding
 import com.searchdirectly.searchword.presentation.fragments.BookmarkFragment
 import com.searchdirectly.searchword.presentation.fragments.HomeFragment
-import com.searchdirectly.searchword.presentation.fragments.RecentSearchFragment
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,16 +31,29 @@ class MainActivity : AppCompatActivity() {
                     loadFragment(HomeFragment())
                     true
                 }
-                R.id.action_recent -> {
-                    loadFragment(RecentSearchFragment())
-                    true
-                }
                 R.id.action_bookmark -> {
                     loadFragment(BookmarkFragment())
                     true
                 }
+                R.id.action_share -> {
+                    true
+                }
+                R.id.action_save -> {
+                    Toast.makeText(this,"Save",Toast.LENGTH_SHORT).show()
+                   // binding.bottomNavigation.menu.findItem(R.id.action_bookmark).isVisible = true
+                    true
+                }
+                R.id.action_refresh -> {
+                    //binding.bottomNavigation.menu.findItem(R.id.action_bookmark).isVisible = false
+                    true
+                }
                 else -> {
-                    TODO()
+                    Toast.makeText(
+                        applicationContext,
+                        "Operation not implemented",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
                 }
             }
         }
@@ -55,38 +63,6 @@ class MainActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.frameLayout, fragment)
         transaction.commit()
-    }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-           menuInflater.inflate(R.menu.main_menu, menu)
-           val search = menu?.findItem(R.id.action_search)
-           val searchView = search?.actionView as SearchView
-           searchView.queryHint = "Type phrase and choose website to search"
-           searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-               override fun onQueryTextSubmit(query: String?): Boolean {
-                   //show dialog(about search phrase), if text is not null and chipItem is selected
-                   return false
-               }
-
-               override fun onQueryTextChange(newText: String?): Boolean {
-                   // adapter.filter.filter(newText)
-                   return true
-               }
-           })
-           return super.onCreateOptionsMenu(menu)
-       }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_share -> {
-                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.action_save -> {
-                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
 
--- Searchword/app/src/main/java/com/searchdirectly/searchword/presentation/fragments/HomeFragment.kt
+++ Searchword/app/src/main/java/com/searchdirectly/searchword/presentation/fragments/HomeFragment.kt
@@ -1,33 +1,18 @@
 package com.searchdirectly.searchword.presentation.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import com.searchdirectly.searchword.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setHasOptionsMenu(true)
+
     }
 
     override fun onCreateView(
@@ -38,23 +23,32 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+        val search = menu.findItem(R.id.action_search)
+        val searchView = search?.actionView as SearchView
+        searchView.queryHint = "Type phrase and choose website to search"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //show dialog(about search phrase), if text is not null and chipItem is selected
+                return false
             }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // adapter.filter.filter(newText)
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu,inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_about_app -> {
+                Toast.makeText(requireContext(), "Item 1 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
